fc151f8615690f4aee5e79f96f1133427d8cffd8
Nettle <ctmann@gmail.com>
2019-02-23 17:35:12 -0500

    No Year money?

--- Code/Import_and_Cleaning.R
+++ Code/Import_and_Cleaning.R
@@ -9,8 +9,14 @@
 library(tidyverse)
 library(readxl)
 library(arsenal)
+library(stringr)
+library(rpivotTable)
 
 # Functions and Common Vars -----------------------------------------------
+  iferror <- function(expr, error_expr){
+  tryCatch(expr, error=function(e){error_expr})
+}     
+      
 
 
 # How to Update this file -------------------------------------------------
@@ -26,6 +32,7 @@ current.fy <- 2018
 #' 2. Download data to tibble:  (downloading directly with read_excel is not currently possible)
 #' 3. unnest
 
+
 # 1. Create tibble
       sf.tibble <- tibble(
         report.FY = c(2013:current.fy),
@@ -39,6 +46,7 @@ current.fy <- 2018
                           #< new hyperlink goes here #
                       ))
 
+      
 # 2.  Download and Read
 
       # Download Excel Files to Raw folder
@@ -47,48 +55,98 @@ current.fy <- 2018
       
       Map(my.download.function, sf.tibble$hyperlink, sf.tibble$my.filename)
       
-      
       # Read files to tibble
       sf.tibble.2 <- sf.tibble %>% 
         mutate(my.data = map(my.filename, ~(.x %>% 
                                               read_excel(sheet="Raw Data", col_types="text") ) ) )
-                                      
-
+             
+                             
 # 3.  Unnest and initial tidy
       sf.tibble.3 <- sf.tibble.2 %>% 
         unnest() %>% 
         group_by(report.FY) %>% 
         mutate(sorting.id = row_number() ) %>% 
-        select(sorting.id, everything())
+        select(sorting.id, everything()) %>% ungroup()
 
-View(sf.tibble.3)
+      # Remove (some) inconsistent columns
+      sf.tibble.4 <- sf.tibble.3 %>% 
+        select(-c(#ALLOC,
+                  COHORT,
+                  CAT_B) ) 
+      # NAs expose inconsistent formatting
+        # sf.tibble.4 %>% summarise_all(funs(sum(is.na(.)) )) %>% View()
+      
+# Basic Tidying ----------------------------------------------------------------
 
+      # Convert and Trim
+      sf.tibble.5 <- sf.tibble.4 %>% 
+        # Trim strings
+        mutate_if(is.character, str_trim) %>% 
+        # convert numbers
+        mutate_at(vars(contains(match="AMT")), funs(parse_number) ) %>% 
+        # convert dates
+        mutate(LAST_UPDATED = parse_date(LAST_UPDATED, "%Y-%m-%d") )
+      
+      
+# Add Metadata ------------------------------------------------------------
+#' 1 account.name
+#' 2 service
+#' 3 ac.rc
+#' 4 life.FY.begin
+#' 5 life.FY.end
+#' 6 life.of.money
+#' 7 lifespan.of.money
+#' 8 FY.cancelled.calculated
 
+      # 1,2: Account name and service
+      sf.tibble.6 <- sf.tibble.5 %>% 
+        mutate(
+          account.name = str_sub(OMB_ACCT, start=15),
+          service = case_when(
+              #TRAG 12 is not Dept. of Agriculture; TRAG 69 is not Dept.Transportation
+              TRAG %in% "12"| TRAG %in% "21" | str_detect(account.name, "Army") | ALLOC %in% "21"          ~ "Army",
+              str_detect(account.name, "Marine Corps") & !str_detect(account.name, "Navy")                 ~ "Marine.Corps",
+              TRAG %in% "17" | ALLOC %in% "17" | str_detect(account.name, "Navy")                          ~ "Navy",
+              TRAG %in% "57" | ALLOC %in% "57" | str_detect(account.name, "Air Force")                     ~ "Air.Force",
+              TRAG %in% "97" | ALLOC %in% "97" | str_detect(account.name, "Defense-Wide")                  ~ "Defense.Wide",
+              TRUE                                                                                         ~ "unknown"
+              ) ) 
 
+    # 3. ac.or.rc  
+    sf.tibble.7 <- sf.tibble.6 %>% 
+      mutate(ac.rc = case_when(
+        service %in% c("Army", "Marine.Corps", "Navy", "Air.Force") & 
+          (str_detect(OMB_ACCOUNT, "Reserve")|str_detect(OMB_ACCOUNT, "National Gu") ) ~ "RC",
+        service %in% c("Army", "Marine.Corps", "Navy", "Air.Force")              ~ "AC",
+        TRUE ~ "Other") ) 
+            
+   # 4,5,
+     sf.tibble.8 <-  sf.tibble.7 %>% 
+        mutate(
+            # Helper life cols
+          helper.FY.begin = ifelse(!is.na(FY1), 
+                                   FY1,  
+                                   FY2), 
+          helper.FY.end   = FY2,
+            # life.begin
+          life.FY.begin = case_when(
+            str_detect(helper.FY.begin, "X")      ~ helper.FY.begin,
+            parse_number(helper.FY.begin) >= 50   ~  paste0("19",  helper.FY.begin),
+            parse_number(helper.FY.begin) <  50   ~  paste0("20",  helper.FY.begin)) ,
+            # life.end
+          life.FY.end = case_when(
+            str_detect(helper.FY.end, "X")      ~ "X",
+            parse_number(helper.FY.end) >= 50   ~  paste0("19",  helper.FY.end),
+            parse_number(helper.FY.end) <  50   ~  paste0("20",  helper.FY.end))
+          ) %>%  
+       mutate(life.of.money = iferror(parse_number(helper.FY.end) - parse_number(helper.FY.begin)+1, helper.FY.end )) %>% 
+       mutate(lifespan.of.money = paste0(life.FY.begin, "/", life.FY.end) ) %>% 
+       mutate(FY.cancelled = iferror(parse_number(life.FY.begin) +5, "X") )
+       
 
+# export ------------------------------------------------------------------
 
-
-
-x.2017 <- sf.tibble.2 %>% filter(report.FY %in% "2018") %>% unnest()
-x.2018 <- sf.tibble.2 %>% filter(report.FY %in% "2017") %>% unnest()
-
-setdiff(names(x.2017), names(x.2018) )
-
-compare(x.2017, x.2018)
-
-# download to temp file
-tmp <- tempfile(fileext = ".xlsx")
-
-# download one
-sf.tibble$hyperlink[1:2]
-
-
-httr::GET(url = sf.tibble$hyperlink[1:2],
-            write_disk( tmp) )
-
-excel_sheets(path=tmp)
-read_excel(path=tmp, sheet="Raw Data", col_types="text")
-
+     
 
 
 
--- README.md
+++ README.md
@@ -1,20 +1,33 @@
 # sf133
 
-#### Data Definitions
-[OMB circular A-11, Appendix F] (https://obamawhitehouse.archives.gov/sites/default/files/omb/assets/a11_current_year/app_f.pdf) contains data definitions for SF133.
+### Data Definitions
+(OMB circular A-11, Appendix F)[https://obamawhitehouse.archives.gov/sites/default/files/omb/assets/a11_current_year/app_f.pdf] contains data definitions for SF133.
 
-#### Formats
+### Formats
 In FY2018, OMB began providing a quarterly report, along with separate monthly (.xlsx)
 In FY2013, OMB began consistenly posting Excel files on monthly basis. (.xlsx)
 In FY2012, OMB reported Nov, Jul, and Aug periods in Excel. (.xls)
 In previous years (FY1998-FY2011), only pdfs are available. (.pdf)
 
 
-#### Shape
+### Shape
 From FY2013-FY2017: 36 Variables
+
 In FY2018, 4 new colnames introduced
-*"AMT1"            
-*"AMT2"            
-*"AMT3"            
-*"AMT4"            
-*"LINE_DESC_SHORT"
+* "AMT1"            
+* "AMT2"            
+* "AMT3"            
+* "AMT4"            
+* "LINE_DESC_SHORT"
+
+In FY2017, AMT_OCT (not included in previous years) introduced
+
+### Data Inconsistencies
+TRAG (treasury agency code) wrongly identifies:
+* 12 (dept. of agriculture) and 
+* 69 (Dept.Transportation)
+
+OMB_ACCOUNT cuts off long titles (as "Medicare-Eligible REtiree Health Fund Contribution, National Gua")
+
+## Questions
+* How can an account have no begin year, and "X" end year? (Non financing account?)
